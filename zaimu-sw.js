9c79fe2560897b400f5fc4cc9de3d270a63676ac
fujisan-fujisan <fujisan.for.twitter@gmail.com>
2026-03-28 10:41:18 +0900

    Add files via upload

---
+++ zaimu-sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'zaimu-keisan-v1';
+const FILES = [
+  '/zaimu-keisan.html',
+  '/zaimu-manifest.json',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
